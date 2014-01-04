807c6020f1c56e222c3b5d60e5e9082d8822c8f6
Joe Kuemerle <joe@kuemerle.com>
2014-01-04 15:20:54 -0500

    Protocolless img

--- FlickrSource/FlickrSource.cs
+++ FlickrSource/FlickrSource.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.Composition;
+using System.Text.RegularExpressions;
 
 using FlickrNet;
 using DoorComp.Common;
 
 namespace FlickrSource
 {
+    public static class FlickrSourceExtensions
+    {
+        public static string RemoveProtocol(this string val)
+        {
+            var ret = val.Replace("http://", "//");
+            ret = ret.Replace("https://", "//");
+            return ret;
+        }
+
+    }
+
     [Export(typeof(IPictureSource))]
     public class FlickrSource : IPictureSource
     {
@@ -42,8 +54,9 @@ namespace FlickrSource
         {
             var retVal = new List<PictureInfo>();
             PhotoSearchOptions options = new PhotoSearchOptions() { Tags = SearchString, TagMode= TagMode.AllTags };
-            retVal = (from a in _flickr.PhotosSearch(options) select new PictureInfo() { ID = a.PhotoId, FullSizeURL = a.MediumUrl, 
-                ThumbnailURL = a.ThumbnailUrl, Title = a.Title, URL = a.WebUrl, MediumURL = a.MediumUrl }).ToList();
+            retVal = (from a in _flickr.PhotosSearch(options) select new PictureInfo() { ID = a.PhotoId, 
+                FullSizeURL = a.MediumUrl.RemoveProtocol(), ThumbnailURL = a.ThumbnailUrl.RemoveProtocol(), 
+                Title = a.Title, URL = a.WebUrl.RemoveProtocol(), MediumURL = a.MediumUrl.RemoveProtocol() }).ToList();
             return retVal;
         }
 
@@ -54,10 +67,11 @@ namespace FlickrSource
             if(null != pic)
             {
                 retVal.ID = pic.PhotoId;
-                retVal.FullSizeURL = pic.MediumUrl;
-                retVal.ThumbnailURL = pic.ThumbnailUrl;
+                retVal.FullSizeURL = pic.MediumUrl.RemoveProtocol();
+                retVal.ThumbnailURL = pic.ThumbnailUrl.RemoveProtocol();
                 retVal.Title = pic.Title;
-                retVal.URL = pic.WebUrl;
+                retVal.URL = pic.WebUrl.RemoveProtocol();
+                retVal.MediumURL = pic.MediumUrl.RemoveProtocol();
             }
             return retVal;
         }
